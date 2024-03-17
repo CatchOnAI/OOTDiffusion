1c7578d8054c17dc10d30f94b075110b1720b18c
Ubuntu <ubuntu@ip-172-31-14-249.us-west-2.compute.internal>
2024-03-17 00:05:28 +0000

    add a clip script

--- controlnet/scripts/image2text.py
+++ controlnet/scripts/image2text.py
@@ -1,38 +1,47 @@
-import os
-import torch
-import clip
 from PIL import Image
+import json  
+import os 
+import random 
+import shutil
+from clip_interrogator import Config, Interrogator
 
-# Set the path to the directory containing the images
-image_dir = "../data/VITON-HD/train/cloth"
 
-# Load the pre-trained CLIP model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
+vtol_folder_path = "controlnet/data/VITON-HD/"
+test_folder_path = f"{vtol_folder_path}/test"
+train_folder_path = f"{vtol_folder_path}/train"
 
-# Iterate over the images in the directory
-for filename in os.listdir(image_dir):
-    if filename.endswith(".jpg") or filename.endswith(".png"):  # Adjust the extensions as needed
-        # Load the image using PIL
-        image_path = os.path.join(image_dir, filename)
-        image = Image.open(image_path)
-        
-        # Preprocess the image for CLIP
-        image_input = preprocess(image).unsqueeze(0).to(device)
-        
-        # Generate the text representation using CLIP
-        with torch.no_grad():
-            image_features = model.encode_image(image_input)
-            text_features = model.encode_text(clip.tokenize(["a photo of a piece of clothing"]).to(device))
-            similarity = torch.cosine_similarity(image_features, text_features)
-            text_repr = similarity.item()
-        
-        print(str(text_repr))
-        
-        # Save the text representation to a file
-        # text_filename = os.path.splitext(filename)[0] + ".txt"
-        # text_path = os.path.join(image_dir, text_filename)
-        # with open(text_path, "w") as file:
-        #     file.write(str(text_repr))
 
-print("Image to text conversion completed.")
+all_cloth_names = [img_name for img_name in os.listdir(f"{vtol_folder_path}/test/cloth")]
+sub_cloth_names = set(random.choices(all_cloth_names, k=128))
+
+percent = 20 
+
+def keep_certain_rows(file_path, condition, newfile_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Keep only lines that meet the condition
+    # kept_lines = [' '.join([line.split(' ')[1][:-1], line.split(' ')[1][:-1]+'\n']) for line in lines if condition(line)]
+    kept_lines = [line for line in lines if condition(line)]
+
+    # Write the kept lines to a new file
+    with open(newfile_path, 'w') as file:
+        file.writelines(kept_lines)
+
+# Define your condition here
+def condition(line):
+    # return line.split(' ')[1][:-1] in sub_cloth_names
+    return random.random() < percent/100
+
+# Call the function
+keep_certain_rows(f'{vtol_folder_path}/train_pairs.txt', condition, f'{vtol_folder_path}/subtrain_{percent}.txt')
+
+
+images_path = f"{test_folder_path}/cloth"
+ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
+
+result = {}
+for cloth_name in sub_cloth_names:
+    image = Image.open(f"{images_path}/{cloth_name}").convert('RGB')
+    caption = ci.interrogate(image)
+    result[cloth_name] = caption
