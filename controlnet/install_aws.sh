310894647aaf4199b8036b583b9f23b5c18fe95c
Ubuntu <ubuntu@ip-172-31-12-46.us-west-2.compute.internal>
2024-03-18 00:24:36 +0000

    git cache cleared

--- controlnet/install_aws.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-sudo apt update
-sudo apt-get --assume-yes install unzip
-
-# yum remove awscli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "./awscliv2.zip"
-unzip ./awscliv2.zip -d .
-./aws/install
-rm -rf ./awscliv2.zip
